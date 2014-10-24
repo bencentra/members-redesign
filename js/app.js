44ad1625bd8019921228e91ca1988d91409338b8
Ben Centra <bcentra@constantcontact.com>
2014-10-23 21:53:29 -0400

    added homepage, templates for navbar and meetings

---
+++ js/app.js
@@ -0,0 +1,35 @@
+var app = angular.module("members", []);
+
+app.directive("navbar", function() {
+  return {
+    restrict: "E",
+    templateUrl: "templates/navbar.html"
+  }
+});
+
+app.directive("meetings", function() {
+  return {
+    restrict: "E",
+    templateUrl: "templates/meetings.html",
+    scope: {
+      meetings: "=data"
+    }
+  };
+});
+
+app.controller("MembersController", ['$scope', '$http', function($scope, $http) {
+  // Get the meeting times
+  $scope.meetings = [];
+  $http.get("./meetings.json").success(function (response) {
+    $scope.meetings = response;
+  }).error(function (error) {
+    console.error("Error getting meetings.json");
+  });
+  // Get all the links
+  $scope.sections = [];
+  $http.get("./links.json").success(function (response) {
+    $scope.sections = response;
+  }).error(function (error) {
+    console.error("Error getting links.json");
+  });
+}]);
