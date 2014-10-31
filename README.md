27a2be27f6e4834e365928ea5c51c7cf66aa5f02
Ben Centra <bcentra@constantcontact.com>
2014-10-31 14:18:39 -0400

    moved data files, adding bootstrap-csh

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 members-redesign
 ================
 
-CSH Members Portal 2: Electric Boogaloo
+CSH Members Portal 2: Electric Boogaloo. Uses [bootstrap-csh](https://github.com/bencentra/bootstrap-csh) for styles.
--- js/app.js
+++ js/app.js
@@ -20,14 +20,14 @@ app.directive("meetings", function() {
 app.controller("MembersController", ['$scope', '$http', function($scope, $http) {
   // Get the meeting times
   $scope.meetings = [];
-  $http.get("./meetings.json").success(function (response) {
+  $http.get("./data/meetings.json").success(function (response) {
     $scope.meetings = response;
   }).error(function (error) {
     console.error("Error getting meetings.json");
   });
   // Get all the links
   $scope.sections = [];
-  $http.get("./links.json").success(function (response) {
+  $http.get("./data/links.json").success(function (response) {
     $scope.sections = response;
   }).error(function (error) {
     console.error("Error getting links.json");
